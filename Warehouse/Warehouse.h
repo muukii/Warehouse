e33063156be006f9879b3ac6016ca10c3069f0d3
muukii <muukii.muukii@gmail.com>
2014-10-05 01:18:42 +0900

    Initial Commit

---
+++ Warehouse/Warehouse.h
@@ -0,0 +1,19 @@
+//
+//  Warehouse.h
+//  Warehouse
+//
+//  Created by Muukii on 10/5/14.
+//  Copyright (c) 2014 Muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Warehouse.
+FOUNDATION_EXPORT double WarehouseVersionNumber;
+
+//! Project version string for Warehouse.
+FOUNDATION_EXPORT const unsigned char WarehouseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Warehouse/PublicHeader.h>
+
+
