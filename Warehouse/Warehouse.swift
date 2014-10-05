eeda0673c2118dc33445f6152109a60639b5a79a
muukii <muukii.muukii@gmail.com>
2014-10-05 16:03:49 +0900

    Fix import

--- Warehouse/Warehouse.swift
+++ Warehouse/Warehouse.swift
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import UIKit
+import Foundation
 
 public class Warehouse: NSObject {
     public enum DirectoryType {
